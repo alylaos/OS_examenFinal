989b3d932bf71ec659d405d0e0738c025347c7ce
unknown <aly_laos@outlook.com>
2024-12-01 00:27:52 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
